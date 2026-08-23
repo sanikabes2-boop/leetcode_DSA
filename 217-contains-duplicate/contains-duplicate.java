c3d3bb35267a318643678f39a70860575414855f
sanikabes2-boop <sanika.bes2@gmail.com>
2026-08-23 16:05:19 +0530

    Time: 17 ms (85.74%) | Memory: 108 MB (60.50%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,5 +1,3 @@
-import java.util.HashSet;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
@@ -15,3 +13,18 @@ class Solution {
         return false;
     }
 }
+/*
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        return set.size() != nums.length;
+    }
+}
+ */
