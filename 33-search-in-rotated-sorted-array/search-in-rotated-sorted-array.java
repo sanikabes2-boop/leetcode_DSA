169215058fbf7aa8ec3a3e7ecb614ff59dee56fd
sanikabes2-boop <sanika.bes2@gmail.com>
2026-09-06 00:13:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (65.87%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        if(target >= nums[0]){
+            return binarySearch(nums, target, 0, pivot);
+        }
+        return binarySearch(nums, target, pivot+1, nums.length-1);
+    }
+
+    public int findPivot(int[] nums){
+        //here the pivot is 7 -> the left and right side of the pivot -> ascending bass 7 aur 0 hi descending hoga....
+        //first find the pivot
+        int start =0;
+        int end =nums.length -1;
+        while(start <= end){
+            int mid=start +(end-start)/2;
+            //"Is there an element after mid that I can compare with?"
+            if(mid<end && nums[mid] > nums[mid+1] ){
+                return mid;
+            }
+            //This is basically detecting the same drop, but from the other side.
+            //"Is there an element before mid?"
+            if(start<mid && nums[mid] < nums[mid-1]){
+                return mid-1;
+            }
+            //to decide which side contains the pivot.
+            if(nums[start] >= nums[mid]){
+                end= mid -1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return nums.length -1;//array not rotated
+    }
+    public int binarySearch(int[] nums, int target, int low, int high){
+        while (low <= high) {
+            int mid = low + (high - low) / 2; 
+            if (nums[mid] == target) {
+                 return mid; 
+            } 
+            if (nums[mid] < target) { 
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            } 
+        } 
+        return -1;
+    }
+}
