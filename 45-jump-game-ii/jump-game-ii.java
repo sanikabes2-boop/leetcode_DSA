1602526d899be1507afdaad90012bca246afc4ea
sanikabes2-boop <sanika.bes2@gmail.com>
2026-09-18 21:19:41 +0530

    Time: 1 ms (99.80%) | Memory: 47.5 MB (21.14%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,16 +1,14 @@
 class Solution {
     public int jump(int[] nums) {
-
         int jumps = 0;
         int currentEnd = 0;
         int farthest = 0;
 
         for (int i = 0; i < nums.length - 1; i++) {
 
-            // Find the farthest position
             farthest = Math.max(farthest, i + nums[i]);
 
-            // Reached the boundary of current jump
+            //Have I reached the end of the range that my current number of jumps can cover?            
             if (i == currentEnd) {
 
                 jumps++;
@@ -23,3 +21,11 @@ class Solution {
         return jumps;
     }
 }
+/*Instead of deciding exactly which index to jump to, we keep track of the farthest position we can reach with the current number of jumps.
+    index:  0  1  2  3  4
+    nums:   2  3  1  1  4
+    sum:    2  4  3  4  X
+currentEnd=The last index that we can reach using the current number of jumps.
+farthest=what is the farthest index that we could reach with one more jump?
+i + nums[i] = means current index ke phle tk ke steps+ no. of steps we can jump from the current index
+*/
